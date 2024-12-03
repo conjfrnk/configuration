84d2c8194c251178753323e5a981c62b6f9cba7f
Connor Frank <conjfrnk@gmail.com>
2024-12-02 21:11:51 -0500

    disable snipe and add hydra

--- config/.config/doom/packages.el
+++ config/.config/doom/packages.el
@@ -48,4 +48,6 @@
 ;(unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;(unpin! t)
+(package! evil-snipe :disable t)
 (package! obsidian)
+(package! hydra)
