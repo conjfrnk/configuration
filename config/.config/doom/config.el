1e98fbc5664a8e1fcdfe0803f1a02422b61d0941
Connor Frank <conjfrnk@gmail.com>
2024-12-05 15:42:41 -0500

    improvements

--- config/.config/doom/config.el
+++ config/.config/doom/config.el
@@ -1,3 +1,37 @@
+;; Performance improvements
+(use-package! gcmh
+  :init
+  ;; Increase garbage collection threshold during startup
+  (setq gc-cons-threshold most-positive-fixnum
+        gc-cons-percentage 0.6)
+
+  ;; More aggressive settings for GCMH
+  (setq gcmh-idle-delay 10  ; slightly longer idle delay
+        gcmh-high-cons-threshold (* 512 1024 1024)  ; 512MB during idle
+        gcmh-verbose nil  ; reduce logging noise
+        gcmh-low-cons-threshold (* 128 1024 1024))  ; 128MB active threshold
+  :config
+  ;; Start garbage collection mode
+  (gcmh-mode 1)
+
+  ;; Additional optimizations
+  (add-hook! 'after-init-hook
+    (setq gc-cons-threshold (* 256 1024 1024)))) ; 256MB after init
+
+;; Faster file-name completion
+(setq read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t
+      completion-ignore-case t)
+
+;; Disable bidirectional text scanning
+(setq-default bidi-paragraph-direction 'left-to-right)
+(setq bidi-inhibit-bpa t)
+
+;; LSP performance improvements
+(setq lsp-idle-delay 0.500
+      lsp-log-io nil
+      lsp-completion-provider :capf)
+
 ;; Personal Information
 (setq user-full-name "Connor Frank"
       user-mail-address "conjfrnk@gmail.com")
@@ -5,11 +39,17 @@
 ;; Theme Configuration
 (setq doom-theme 'doom-gruvbox)
 
+;; autosave and backup
+(setq auto-save-default t
+      make-backup-files t)
+
 ;; Line Numbers Configuration
 (setq display-line-numbers-type 'relative)
 
 ;; Enable global auto-revert
 (global-auto-revert-mode t)
+
+(pixel-scroll-precision-mode 1)
 
 ;; Org Configuration
 (after! org
@@ -18,7 +58,81 @@
         (append (directory-files-recursively "~/Notes/org/" "\\.org$")
                 (directory-files-recursively "~/Notes/org/roam/" "\\.org$")))
   (setq org-track-ordered-property-with-tag t)
-  (setq org-agenda-log-mode-items '(closed clock state)))
+  (setq org-agenda-log-mode-items '(closed clock state))
+  (setq org-use-property-inheritance t) ; fix weird issue with src blocks
+  (setq org-startup-with-inline-images t)
+  (setq org-hide-emphasis-markers t)
+  (setq org-edit-src-content-indentation 0)
+  (setq org-startup-with-latex-preview t))
+
+(after! org
+  (custom-set-faces!
+    `((org-document-title)
+      :foreground ,(face-attribute 'org-document-title :foreground)
+      :height 1.3 :weight bold)
+    `((org-level-1)
+      :foreground ,(face-attribute 'outline-1 :foreground)
+      :height 1.1 :weight medium)
+    `((org-level-2)
+      :foreground ,(face-attribute 'outline-2 :foreground)
+      :weight medium)
+    `((org-level-3)
+      :foreground ,(face-attribute 'outline-3 :foreground)
+      :weight medium)
+    `((org-level-4)
+      :foreground ,(face-attribute 'outline-4 :foreground)
+      :weight medium)
+    `((org-level-5)
+      :foreground ,(face-attribute 'outline-5 :foreground)
+      :weight medium)))
+
+(after! org
+  (add-to-list 'org-latex-packages-alist '("" "amsmath" t))
+  (add-to-list 'org-latex-packages-alist '("" "amssymb" t))
+  (add-to-list 'org-latex-packages-alist '("" "mathtools" t))
+  (add-to-list 'org-latex-packages-alist '("" "mathrsfs" t)))
+
+(use-package! org-latex-preview
+  :after org
+  :config
+  (plist-put org-latex-preview-appearance-options
+             :page-width 0.8)
+  (add-hook 'org-mode-hook 'org-latex-preview-auto-mode)
+  (setq org-latex-preview-auto-ignored-commands
+        '(next-line previous-line mwheel-scroll
+          scroll-up-command scroll-down-command))
+  (setq org-latex-preview-numbered t)
+  (setq org-latex-preview-live t)
+  (setq org-latex-preview-live-debounce 0.25))
+
+
+;; Function to find nodes excluding archives
+(defun my/org-roam-node-find-no-archive ()
+  (interactive)
+  (org-roam-node-find nil nil
+                      (lambda (node)
+                        (not (member "archive" (org-roam-node-tags node))))))
+(use-package! org-modern
+  :after org
+  :config
+  (setq
+   org-auto-align-tags t
+   org-tags-column 0
+   org-fold-catch-invisible-edits 'show-and-error
+   org-special-ctrl-a/e t
+   org-insert-heading-respect-content t
+
+   ;; agenda
+   org-agenda-tags-column 0
+   org-agenda-block-separator ?─
+   org-agenda-time-grid
+   '((daily today require-timed)
+     (800 1000 1200 1400 1600 1800 2000)
+     " ┄┄┄┄┄ " "┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄")
+   org-agenda-current-time-string
+   "⭠ now ─────────────────────────────────────────────────")
+
+  (global-org-modern-mode))
 
 ;; Org-roam Configuration
 (use-package! org-roam
@@ -40,14 +154,6 @@
   :config
 
   (org-roam-db-autosync-mode 1)
-
-  ;; Function to find nodes excluding archives
-  (defun my/org-roam-node-find-no-archive ()
-    "Find and open an Org-roam node, excluding archived nodes."
-    (interactive)
-    (org-roam-node-find nil nil
-                        (lambda (node)
-                          (not (member "archive" (org-roam-node-tags node))))))
 
   ;; Class tags functions
   (defun my/get-class-slugs ()
@@ -75,8 +181,8 @@
            (orig-dir (file-name-directory orig-file))
            (archive-dir (expand-file-name "archive/" org-roam-directory))
            (new-dir (replace-regexp-in-string (regexp-quote (expand-file-name org-roam-directory))
-                                            archive-dir
-                                            orig-dir))
+                                              archive-dir
+                                              orig-dir))
            (new-file (expand-file-name (file-name-nondirectory orig-file) new-dir)))
       (unless (file-directory-p new-dir)
         (make-directory new-dir t))
@@ -99,32 +205,32 @@
         '(("p" "Project" plain
            "%?"
            :target (file+head "projects/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :project:\n\n")
+                              "#+title: ${title}\n#+filetags: :project:\n\n")
            :unnarrowed t)
           ("a" "Area" plain
            "%?"
            :target (file+head "areas/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :area:\n\n")
+                              "#+title: ${title}\n#+filetags: :area:\n\n")
            :unnarrowed t)
           ("r" "Resource" plain
            "%?"
            :target (file+head "resources/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :resource:\n\n")
+                              "#+title: ${title}\n#+filetags: :resource:\n\n")
            :unnarrowed t)
           ("A" "Archive" plain
            "%?"
            :target (file+head "archive/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :archive:\n\n")
+                              "#+title: ${title}\n#+filetags: :archive:\n\n")
            :unnarrowed t)
           ("z" "Zettel" plain
            "%?"
            :target (file+head "resources/zettels/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :zettel:\n\n")
+                              "#+title: ${title}\n#+filetags: :zettel:\n\n")
            :unnarrowed t)
           ("c" "Class (Project)" plain
            "%?"
            :target (file+head "projects/classes/${slug}.org"
-                             "#+title: ${title}\n#+filetags: :project:class:${slug}:\n\n")
+                              "#+title: ${title}\n#+filetags: :project:class:${slug}:\n\n")
            :unnarrowed t)
           ("n" "Note" plain
            "%?"
@@ -178,3 +284,15 @@
 
 (after! yasnippet
   (setq yas-snippet-revival nil))
+
+;; LaTeX
+                                        ; use cdlatex completion instead of yasnippet
+(map! :map cdlatex-mode-map
+      :i "TAB" #'cdlatex-tab)
+(map! :after latex
+      :map cdlatex-mode-map
+      :localleader
+      :desc "Insert math symbol"
+      "i" #'cdlatex-math-symbol
+      :desc "Begin environment"
+      "e" #'cdlatex-environment)
--- config/.config/doom/packages.el
+++ config/.config/doom/packages.el
@@ -50,7 +50,32 @@
 ;(unpin! t)
 (package! evil-snipe :disable t)
 (package! obsidian)
-(package! org-roam)
-(package! org-roam-ui
-  :recipe (:host github :repo "org-roam/org-roam-ui"))
 (package! emacsql-sqlite3)
+
+(package! org :recipe
+  (:host nil :repo "https://git.tecosaur.net/mirrors/org-mode.git" :remote "mirror" :fork
+         (:host nil :repo "https://git.tecosaur.net/tec/org-mode.git" :branch "dev" :remote "tecosaur")
+         :files
+         (:defaults "etc")
+         :build t :pre-build
+         (with-temp-file "org-version.el"
+           (require 'lisp-mnt)
+           (let
+               ((version
+                 (with-temp-buffer
+                   (insert-file-contents "lisp/org.el")
+                   (lm-header "version")))
+                (git-version
+                 (string-trim
+                  (with-temp-buffer
+                    (call-process "git" nil t nil "rev-parse" "--short" "HEAD")
+                    (buffer-string)))))
+             (insert
+              (format "(defun org-release () \"The release version of Org.\" %S)\n" version)
+              (format "(defun org-git-version () \"The truncate git commit hash of Org mode.\" %S)\n" git-version)
+              "(provide 'org-version)\n"))))
+  :pin nil)
+(unpin! org)
+(package! org-roam)
+(package! org-modern)
+(package! org-roam-ui)
